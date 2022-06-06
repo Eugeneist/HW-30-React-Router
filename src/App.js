e528c29e00f3624851021e2cdfb3adb8fcd3682e
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-06-06 09:06:32 +0300

    routes fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Flights from "./pages/Flights";
+import Home from "./pages/Home";
+import Hotels from "./pages/Hotels";
+import Packages from "./pages/Packages";
+import Apartments from "./pages/Apartments";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="flights" element={<Flights />} />
+
+        <Route path="hotels" element={<Hotels />}>
+          <Route path=":apartmentsId" element={<Apartments />} />
+        </Route>
+
+        <Route path="packages" element={<Packages />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,48 @@
-import { Routes, Route, NavLink } from "react-router-dom";
-import Flights from "../pages/Flights";
-import Home from "../pages/Home";
-import Hotels from "../pages/Hotels";
-import Packages from "../pages/Packages";
-import Apartments from "../pages/Apartments";
-import NotFound from "../pages/NotFound";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <header>
-        <nav className="menu">
-          <ul className="menu__list">
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "navlink")}
-                to="/"
-              >
-                HOME PAGE
-              </NavLink>
-            </li>
+    <header>
+      <nav className="menu">
+        <ul className="menu__list">
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navlink")}
+              to="/"
+            >
+              HOME PAGE
+            </NavLink>
+          </li>
 
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "navlink")}
-                to="flights"
-              >
-                FLIGHTS
-              </NavLink>
-            </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navlink")}
+              to="flights"
+            >
+              FLIGHTS
+            </NavLink>
+          </li>
 
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "navlink")}
-                to="hotels"
-              >
-                HOTELS
-              </NavLink>
-            </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navlink")}
+              to="hotels"
+            >
+              HOTELS
+            </NavLink>
+          </li>
 
-            <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "active" : "navlink")}
-                to="packages"
-              >
-                PACKAGES
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="flights" element={<Flights />} />
-
-        <Route path="hotels" element={<Hotels />}>
-          <Route path=":apartmentsId" element={<Apartments />} />
-        </Route>
-
-        <Route path="packages" element={<Packages />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navlink")}
+              to="packages"
+            >
+              PACKAGES
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
